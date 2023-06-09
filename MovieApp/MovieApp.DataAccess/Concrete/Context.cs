5ddc2796988d4ed098bfc412ac61fb8b9495dbd5
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-06-09 14:38:15 +0300

    Created layers and folders. Added context class.

---
+++ MovieApp/MovieApp.DataAccess/Concrete/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.DataAccess.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("server =(localdb)\\MSSQLLocalDB;database=MovieBackend;Trusted_Connection=true;TrustServerCertificate=true");
+
+        }
+
+
+
+
+    }
+}
---
+++ MovieApp/MovieApp.Panel.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieApp.Panel.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
