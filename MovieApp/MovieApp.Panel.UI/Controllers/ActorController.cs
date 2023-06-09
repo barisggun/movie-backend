1ce1a001fc28c78c426bdfc2880fccabad9d09a9
isilaycakar <118280073+isilaycakar@users.noreply.github.com>
2023-06-09 17:26:27 +0300

    actor controller

--- MovieApp/MovieApp.Panel.UI/Controllers/ActorController.cs
+++ MovieApp/MovieApp.Panel.UI/Controllers/ActorController.cs
@@ -1,17 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.BusinessLayer.Concrete;
+using MovieApp.DataAccess.EntityFramework;
+using MovieApp.EntityLayer.Entities;
 
 namespace MovieApp.Panel.UI.Controllers
 {
     public class ActorController : Controller
     {
+        ActorManager actorManager = new ActorManager(new EfActorRepository());
+
+        public ActorController(ActorManager actorManager)
+        {
+            this.actorManager = actorManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var values = actorManager.GetAll();
+            return View(values);
         }
 
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Actor actor)
+        {
+            actorManager.Create(actor);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            Actor actor = actorManager.GetById(id);
+            return View(actor);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteCategory(Actor actor)
+        {
+            actorManager.Delete(actor);
+            return RedirectToAction("Index");
         }
     }
 }
