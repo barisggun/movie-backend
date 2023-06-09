82c49683b6cb1f54fb102347b8123c6b2bbec1f1
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-06-09 16:13:13 +0300

    Generic class created. AdminLayout added.

---
+++ MovieApp/MovieApp.DataAccess/Abstract/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.DataAccess.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        T GetById(int id);
+        //T GetOne(Expression<Func<T, bool>> filter);
+        List<T> GetAll();
+
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
