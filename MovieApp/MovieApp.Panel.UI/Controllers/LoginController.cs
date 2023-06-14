9ed5934710aa0a76bbd36d65eb01517e6e8c0874
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-06-14 13:06:03 +0300

    Login view model

---
+++ MovieApp/MovieApp.Panel.UI/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MovieApp.EntityLayer.Entities;
+using MovieApp.Panel.UI.Models;
+
+namespace MovieApp.Panel.UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            this._signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserSignViewModel p)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.username, p.password, false, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Movie");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+            return View();
+        }
+
+    }
+}
---
+++ MovieApp/MovieApp.Panel.UI/Models/UserSignViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApp.Panel.UI.Models
+{
+    public class UserSignViewModel
+    {
+        [Required(ErrorMessage ="Lütfen kullanıcı adını giriniz.")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz.")]
+        public string password { get; set; }
+    }
+}
