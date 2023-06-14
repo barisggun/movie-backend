fe3db347426de141f138dd7d261fa17a7600b53e
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-06-14 22:50:45 +0300

    user layers

---
+++ MovieApp/MovieApp.BusinessLayer/Concrete/UserManager.cs
@@ -0,0 +1,46 @@
+﻿using MovieApp.BusinessLayer.Abstract;
+using MovieApp.DataAccess.Abstract;
+using MovieApp.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.BusinessLayer.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public void Create(AppUser appUser)
+        {
+            _userDal.Create(appUser);
+        }
+
+        public void Delete(AppUser appUser)
+        {
+            _userDal.Delete(appUser);
+        }
+
+        public List<AppUser> GetAll()
+        {
+            return _userDal.GetAll();
+        }
+
+        public AppUser GetById(int id)
+        {
+           return _userDal.GetById(id);
+        }
+
+        public void Update(AppUser appUser)
+        {
+            _userDal.Update(appUser);
+        }
+    }
+}
