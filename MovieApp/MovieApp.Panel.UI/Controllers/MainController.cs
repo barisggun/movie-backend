9b3ecba3a838dfb4e1b0ca5e4e68dea248602665
isilaycakar <118280073+isilaycakar@users.noreply.github.com>
2023-07-06 12:28:40 +0300

    Search işleminin yapılması

--- MovieApp/MovieApp.Panel.UI/Controllers/MainController.cs
+++ MovieApp/MovieApp.Panel.UI/Controllers/MainController.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using MovieApp.BusinessLayer.Concrete;
+using MovieApp.DataAccess.Concrete;
 using MovieApp.DataAccess.EntityFramework;
 using MovieApp.EntityLayer.Entities;
+using MovieApp.Panel.UI.Models;
 
 namespace MovieApp.Panel.UI.Controllers
 {
@@ -14,11 +16,34 @@ namespace MovieApp.Panel.UI.Controllers
         HomepageCoverManager homepageCoverManager = new HomepageCoverManager(new EfHomepageCoverRepository());
         MovieManager mm = new MovieManager(new EfMovieRepository());
         BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context c = new Context();
 
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Search(string searchTerm)
+        {
+            if (string.IsNullOrEmpty(searchTerm) || searchTerm.Trim().Length < 3)
+            {
+                ViewBag.ErrorMessage = "Lütfen geçerli bir arama terimi girin.";
+                
+            }
+
+            var filterMovies = c.Movies.Where(m=>m.MovieTitle.Contains(searchTerm)).ToList();
+            var filterBlogs = c.Blogs.Where(m=>m.BlogContent.Contains(searchTerm) || m.BlogTitle.Contains(searchTerm)).ToList();
+            var filterUsers = c.Users.Where(m => m.UserName.Contains(searchTerm) || m.NameSurname.Contains(searchTerm)).ToList();
+
+            var searchResults = new SearchResultsModel
+            {
+                Movies = filterMovies,
+                Blogs = filterBlogs,
+                Users = filterUsers
+            };
+
+            return View(searchResults);
         }
 
     }
---
+++ MovieApp/MovieApp.Panel.UI/Models/SearchResultsModel.cs
@@ -0,0 +1,12 @@
+﻿using MovieApp.EntityLayer.Entities;
+
+namespace MovieApp.Panel.UI.Models
+{
+    public class SearchResultsModel
+    {
+        public List<Movie> Movies { get; set; }
+        public List<Blog> Blogs { get; set; }
+        public List<AppUser> Users { get; set; }
+
+    }
+}
