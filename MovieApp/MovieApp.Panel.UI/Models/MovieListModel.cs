f28f9960b99d52f97f194e4d07b4d035e294d24d
isilaycakar <118280073+isilaycakar@users.noreply.github.com>
2023-07-10 18:16:02 +0300

    filtreleme işlemlerinin backend kısmının yapılması

---
+++ MovieApp/MovieApp.Panel.UI/Models/MovieListModel.cs
@@ -0,0 +1,16 @@
+﻿using MovieApp.EntityLayer.Entities;
+
+namespace MovieApp.Panel.UI.Models
+{
+    public class MovieListModel
+    {
+        public FilterModel Filter { get; set; }
+        public List<Movie> Movies { get; set; }
+
+        public MovieListModel()
+        {
+            Filter = new FilterModel();
+            Movies = new List<Movie>(); // Movies özelliğini başlat
+        }
+    }
+}
