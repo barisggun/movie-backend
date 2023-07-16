d05de68a7c6bd14aaf833fb9316b1884f86caa08
isilaycakar <118280073+isilaycakar@users.noreply.github.com>
2023-07-16 21:38:59 +0300

    main tasarım dosyaları

---
+++ MovieApp/MovieApp.Panel.UI/ViewComponents/Main/MostLiked.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.BusinessLayer.Concrete;
+using MovieApp.DataAccess.Concrete;
+using MovieApp.DataAccess.EntityFramework;
+
+namespace MovieApp.Panel.UI.ViewComponents.Main
+{
+    public class MostLiked:ViewComponent
+    {
+        MovieManager mm = new MovieManager(new EfMovieRepository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            var values = c.Movies.Include(m => m.Directors).OrderByDescending(m => m.AverageRating).Take(3).ToList();
+            return View(values);
+        }
+    }
+}
---
+++ MovieApp/MovieApp.Panel.UI/wwwroot/main/assets/JS/main.js
@@ -0,0 +1,43 @@
+const hamburger = document.querySelector(".hamburger-icon");
+const hamburgerMenu = document.querySelector(".hamburgerMenu");
+
+hamburger.addEventListener("click", () => {
+    if (hamburgerMenu.style.display === "block") {
+        hamburgerMenu.style.display = "none";
+    } else {
+        hamburgerMenu.style.display = "block";
+    }
+})
+
+var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
+
+var btn1 = document.querySelector('#modal1');
+var btn2 = document.querySelector('#modal2');
+
+var span1 = document.querySelector('#modal1 .close');
+var span2 = document.querySelector('#modal2 .close');
+
+btn1.onclick = function () {
+    modal1.style.display = "block";
+}
+btn2.onclick = function () {
+    modal2.style.display = "block";
+}
+
+span1.onclick = function () {
+    modal1.style.display = "none";
+}
+span2.onclick = function () {
+    modal2.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
+
