c0efc1c84adbad15fb9b937d3aef8180c0edce69
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-08-04 02:21:20 +0300

    Bütün sayfalarda çalışacak olan responsive hamburger menü gösterimi

---
+++ MovieApp/MovieApp.Panel.UI/ViewComponents/Main/HomePageUsernameResponsive.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MovieApp.BusinessLayer.Concrete;
+using MovieApp.DataAccess.Concrete;
+using MovieApp.DataAccess.EntityFramework;
+
+namespace MovieApp.Panel.UI.ViewComponents.Main
+{
+    public class HomePageUsernameResponsive: ViewComponent
+    {
+        UserManager userManager = new UserManager(new EfUserRepository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            var username = User.Identity.Name;
+            var userID = c.Users.Where(x => x.UserName == username).Select(y => y.Id).FirstOrDefault();
+            var values = userManager.GetById(userID);
+            return View(values);
+        }
+    }
+}
