4f68209261d5cfccb2f46404c7706764ace1890b
isilaycakar <118280073+isilaycakar@users.noreply.github.com>
2023-08-05 18:53:31 +0300

    js kod güncellemesi

--- MovieApp/MovieApp.Panel.UI/wwwroot/main/assets/JS/main.js
+++ MovieApp/MovieApp.Panel.UI/wwwroot/main/assets/JS/main.js
@@ -1,45 +1,54 @@
-const hamburger = document.querySelector(".hamburger-icon");
-const hamburgerMenu = document.querySelector(".hamburgerMenu");
+//const hamburger = document.querySelector(".hamburger-icon");
+//const hamburgerMenu = document.querySelector(".hamburgerMenu");
 
-hamburger.addEventListener("click", () => {
-    if (hamburgerMenu.style.display === "block") {
-        hamburgerMenu.style.display = "none";
-    } else {
-        hamburgerMenu.style.display = "block";
+//hamburger.addEventListener("click", () => {
+//    if (hamburgerMenu.style.display === "block") {
+//        hamburgerMenu.style.display = "none";
+//    } else {
+//        hamburgerMenu.style.display = "block";
+//    }
+//})
+document.addEventListener("DOMContentLoaded", function () {
+    const modal1 = document.getElementById("myModal1");
+    const modal2 = document.getElementById("myModal2");
+
+    const btn1 = document.querySelector('#modal1');
+    const btn2 = document.querySelector('#modal2');
+
+    const span1 = document.querySelector('#myModal1 .close');
+    const span2 = document.querySelector('#myModal2 .close');
+
+    // Ýzleme Listem modal pencerelerini açan fonksiyon
+    function openModal1() {
+        modal1.style.display = "block";
     }
-})
 
-var modal1 = document.getElementById("myModal1");
-var modal2 = document.getElementById("myModal2");
+    // Ýzlediklerim modal pencerelerini açan fonksiyon
+    function openModal2() {
+        modal2.style.display = "block";
+    }
 
-var btn1 = document.querySelector('#modal1');
-var btn2 = document.querySelector('#modal2');
+    btn1.onclick = openModal1; // Ýzleme Listem butonuna týklamada modal1 açýlacak
+    btn2.onclick = openModal2; // Ýzlediklerim butonuna týklamada modal2 açýlacak
 
-var span1 = document.querySelector('#modal1 .close');
-var span2 = document.querySelector('#modal2 .close');
-
-btn1.onclick = function () {
-    modal1.style.display = "block";
-}
-btn2.onclick = function () {
-    modal2.style.display = "block";
-}
-
-span1.onclick = function () {
-    modal1.style.display = "none";
-}
-span2.onclick = function () {
-    modal2.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target == modal1) {
+    span1.onclick = function () {
         modal1.style.display = "none";
-    }
-    if (event.target == modal2) {
+    };
+
+    span2.onclick = function () {
         modal2.style.display = "none";
-    }
-}
+    };
+
+    window.onclick = function (event) {
+        if (event.target == modal1) {
+            modal1.style.display = "none";
+        }
+        if (event.target == modal2) {
+            modal2.style.display = "none";
+        }
+    };
+});
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
@@ -55,11 +64,5 @@ function openNavFilter() {
 function closeNavFilter() {
     document.getElementById("mySidenavLeft").style.width = "0";
 }
-const resetButton = document.querySelector("#resetButton");
-const resetFilter = document.querySelectorAll("#reset");
 
-resetButton.addEventListener("click", () => {
-    resetFilter.forEach(resetItem => {
-        resetItem.checked = false;
-    });
-});
+
